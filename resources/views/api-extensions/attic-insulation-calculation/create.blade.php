7485532a50e8ee220b05caa3a4f4005f0667ad39
Abiel Magu <abielmagu.dev@gmail.com>
2023-07-19 01:51:01 -0500

    Actualiza view create de la extencion AIC para mostrar errores de la validacion

--- resources/views/api-extensions/attic-insulation-calculation/create.blade.php
+++ resources/views/api-extensions/attic-insulation-calculation/create.blade.php
@@ -24,7 +24,7 @@ $controls = (object) [
     <div class="col-sm">
         <div class="mb-3">
             <label for="{{ $controls->method->id }}" class="form-label">Method</label>
-            <select class="form-select" id='{{ $controls->method->id }}' name="{{ $controls->method->name }}">
+            <select class="form-select {{ bsInputInvalid( $errors->has($controls->method->name) ) }}" id='{{ $controls->method->id }}' name="{{ $controls->method->name }}">
                 <option disabled selected label="Choose method..."></option>
 
                 @foreach($class::getAllMethods() as $method)                   
@@ -32,6 +32,7 @@ $controls = (object) [
                 @endforeach
 
             </select>
+            <x-error name='{{ $controls->method->name }}'></x-error>
         </div>
     </div>
 
@@ -39,7 +40,7 @@ $controls = (object) [
     <div class="col-sm">
         <div class="mb-3">
             <label for="{{ $controls->rvalue->id }}" class="form-label">R-Value</label>
-            <select class="form-select" id="{{ $controls->rvalue->id }}" name="{{ $controls->rvalue->name }}">
+            <select class="form-select {{ bsInputInvalid( $errors->has($controls->rvalue->name) ) }}" id="{{ $controls->rvalue->id }}" name="{{ $controls->rvalue->name }}">
                 @if(! old( $controls->method->name ) )          
                 <option disabled selected label="Waiting method..."></option>
 
@@ -53,6 +54,7 @@ $controls = (object) [
 
                 @endif
             </select>
+            <x-error name='{{ $controls->rvalue->name }}'></x-error>
 
             @foreach($class::getAllMethodsWithRValues() as $method => $rvalues)                   
             <template id='{{ $class::getWithPrefix( "{$method}RValuesOptionsTemplate" ) }}'>
@@ -69,7 +71,8 @@ $controls = (object) [
     <div class="col-sm">
         <div class="mb-3">
             <label for="{{ $controls->squarefeets->id }}" class="form-label">Square feets</label>
-            <input type="number" class="form-control" min='0' step="0.01" id="{{ $controls->squarefeets->id }}" name="{{ $controls->squarefeets->name }}" value="{{ old($controls->squarefeets->name, 0) }}">
+            <input type="number" class="form-control {{ bsInputInvalid( $errors->has($controls->squarefeets->name) ) }}" min='0' step="0.01" id="{{ $controls->squarefeets->id }}" name="{{ $controls->squarefeets->name }}" value="{{ old($controls->squarefeets->name, 0) }}">
+            <x-error name='{{ $controls->squarefeets->name }}'></x-error>
         </div>
     </div>
 
@@ -82,7 +85,7 @@ $controls = (object) [
                     $class::calculateBags( 
                         old($controls->method->name, 'no-method'), 
                         old($controls->rvalue->name, 'no-rvalue'), 
-                        old($controls->squarefeets->name, 0)
+                        (old($controls->squarefeets->name) ?? 0)
                     )
                 }}
             </div>
