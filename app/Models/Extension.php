c8709897ac5df919795bb2b3c428057efc7d45de
Abiel Magu <abielmagu.dev@gmail.com>
2023-07-27 03:35:53 -0500

    Factoriza atributos del model Extension

--- app/Models/Extension.php
+++ app/Models/Extension.php
@@ -9,6 +9,8 @@ class Extension extends Model
 {
     use HasFactory;
     
+    protected $primaryKey = 'api_extension_id';
+
     protected $fillable = [
         'api_extension_id',
         'name',
@@ -16,7 +18,23 @@ class Extension extends Model
         'description',
     ];
 
-    protected $primaryKey = 'api_extension_id';
+    
+    // Attributes
+
+    public function getControllerClassAttribute()
+    {
+        return sprintf('App\\Http\\Controllers\\ApiExtensions\\%sController', $this->classname);
+    }
+
+    public function getModelClassAttribute()
+    {
+        return sprintf('%s\\ApiExtensions\\%s', __NAMESPACE__, $this->classname);
+    }
+
+    public function formRequestClass(string $form_request_name)
+    {
+        return sprintf('App\\Http\\Requests\\ApiExtensions\\%s\\%s', $this->classname, $form_request_name);
+    }
 
 
     // Relations
