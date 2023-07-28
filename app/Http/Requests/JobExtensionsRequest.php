9406f3530f4b21d1a23a7038e937a82fca87eec5
Abiel Magu <abielmagu.dev@gmail.com>
2023-07-28 05:01:54 -0500

    Corrige y refactoriza validacion de Order para la carga de extensions del job seleccionado

--- app/Http/Requests/JobExtensionsRequest.php
+++ app/Http/Requests/JobExtensionsRequest.php
@@ -27,14 +27,27 @@ class JobExtensionsRequest extends FormRequest
     public function rules()
     {
         return [
-            'job'    => ['bail','required',sprintf('exists:%s,id', \App\Models\Job::class)],
-            'method' => ['bail','required','in:create,edit'],
-            'order'  => ['bail','required_if:method,edit', sprintf('exists:%s,id', \App\Models\Order::class)],
+            'job'    => ['bail', 'required', sprintf('exists:%s,id', \App\Models\Job::class)],
+            'method' => ['bail', 'required', 'in:create,edit'],
+            'order'  => ['bail', 'nullable', 'required_if:method,edit', sprintf('exists:%s,id', \App\Models\Order::class)],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'job.required' => __('Job not present'),
+            'job.exists' => __('Job not exists'),
+            'method.required' => __('Method not present'),
+            'method.in' => __('Method not valid'),
+            'order.required_if' => __('Order not valid'),
+            'order.exists' => __('Order not exists'),
         ];
     }
 
     protected function failedValidation(Validator $validator)
     {
+        // dd($validator->errors());
         throw new HttpResponseException(
             new JsonResponse([
                 'message' => 'Request failed.',
