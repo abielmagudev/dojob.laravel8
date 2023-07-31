f819a87aaf359df56f6e9a3452e7723fec22ecb9
Abiel Magu <abielmagu.dev@gmail.com>
2023-07-31 05:24:00 -0500

    Implementa trait Existential en model Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\CustomTraits\Existential;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    use Existential;
 
     protected $table = 'orders';
     
