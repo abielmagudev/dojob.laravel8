60903782a5325b730200d7ceabe664cf63492752
Abiel Magu <abielmagu.dev@gmail.com>
2023-08-08 00:12:53 -0500

    Factoriza nuevas funciones en trait kernel para api-extensions

--- app/Models/ApiExtensions/Kernel/HasOrderRelation.php
+++ app/Models/ApiExtensions/Kernel/HasOrderRelation.php
@@ -6,8 +6,23 @@ use App\Models\Order;
 
 trait HasOrderRelation
 {
+    public function scopeWhereOrder($query, $id)
+    {
+        return $query->where('order_id', $id);
+    }
+
     public function order()
     {
         return $this->belongsTo(Order::class);
+    }
+
+    public static function prepareToSave(array $inputs, Order $order = null): array
+    {
+        $prepared = method_exists(self::class, 'prepareData') ? self::prepareData(...[$inputs, $order]) : $inputs;
+        
+        if( $order instanceof Order )
+            $prepared['order_id'] = $order->id;
+        
+        return $prepared;
     }
 }
