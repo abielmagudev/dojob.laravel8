9c9275b0b3f03913cfde280783470428c6f706c8
Abiel Magu <abielmagu.dev@gmail.com>
2023-08-09 16:11:43 -0500

    Comenta la funcion de reflashApiExtensionErrors en trait para el controller Order

--- app/Http/Controllers/OrderControllerFeatures/ReflashErrorsTrait.php
+++ app/Http/Controllers/OrderControllerFeatures/ReflashErrorsTrait.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\OrderControllerFeatures;
 
+/**
+ * Reflashes the request validation errors('errors') or error message('danger')
+ * saved in session for the extension forms of the selected job of the order
+ */
 trait ReflashErrorsTrait
 {
     private function reflashApiExtensionErrors()
