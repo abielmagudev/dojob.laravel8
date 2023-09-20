ba9c008fbd37e6b73050dfe89962fe95f53dc69e
Abiel Magu <abielmagu.dev@gmail.com>
2023-09-20 17:13:15 -0500

    Refactoriza y actualiza interface migrable de models ApiExtensions

--- app/Models/ApiExtensions/Kernel/MigratableInterface.php
+++ app/Models/ApiExtensions/Kernel/MigratableInterface.php
@@ -2,25 +2,19 @@
 
 namespace App\Models\ApiExtensions\Kernel;
 
+/**
+ * migrations(): [
+ *    // Base table to migrate (key: string)
+ *    'table_name' => 'migration_path',
+ * 
+ *    // Tables related to the base table (key: string)
+ *    'table_name_n' => 'migration_n_path',
+ * 
+ *    // Modify or update the structure of a table (key: integer)
+ *    n => 'migration_path',
+ * ];
+ */
 interface MigratableInterface
 {
-    /**
-     * return [
-     *    // Base table to migrate (key: string)
-     *    'table_name' => 'migration_path',
-     * 
-     *    // Tables related to the base table (key: string)
-     *    'table_name_n' => 'migration_n_path',
-     * 
-     *    // Modify or update the structure of a table (key: integer)
-     *    'migration_alter_path',
-     * ];
-     */
     public static function migrations(): array;
-
-    public static function install();
-
-    public static function uninstall();
-
-    public static function installed();
 }
