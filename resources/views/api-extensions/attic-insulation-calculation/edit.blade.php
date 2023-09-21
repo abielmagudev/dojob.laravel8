584645605c947825e5a322a2a6420095f16d123b
Abiel Magu <abielmagu.dev@gmail.com>
2023-09-21 08:55:57 -0500

    Corrige function prefix en view edit de AttiInsulationCalculation

--- resources/views/api-extensions/attic-insulation-calculation/edit.blade.php
+++ resources/views/api-extensions/attic-insulation-calculation/edit.blade.php
@@ -1,24 +1,24 @@
 <?php
 $controls = (object) [
     'method' => (object) [
-        'id' => $class::concatPrefix('selectMethod'),
-        'name' => $class::concatPrefix('method'),
+        'id' => $class::prefix('selectMethod'),
+        'name' => $class::prefix('method'),
     ],
     'rvalue' => (object) [
-        'id' => $class::concatPrefix('selectRValue'),
-        'name' => $class::concatPrefix('rvalue'),
+        'id' => $class::prefix('selectRValue'),
+        'name' => $class::prefix('rvalue'),
     ],
     'squarefeets'=> (object) [
-        'id' => $class::concatPrefix('inputSquareFeets'),
-        'name' => $class::concatPrefix('square_feets'),
+        'id' => $class::prefix('inputSquareFeets'),
+        'name' => $class::prefix('square_feets'),
     ],
     'bags' => (object) [
-        'id' => $class::concatPrefix('divBags'),
+        'id' => $class::prefix('divBags'),
     ],
 ];
 ?>
 <h5 class="mb-3">Attic Insulation Calculation</h5>
-<div class="row" id="{{ $class::concatPrefix('atticInsulationCalculationWrapper') }}">
+<div class="row" id="{{ $class::prefix('atticInsulationCalculationWrapper') }}">
     {{-- Methods --}}
     <div class="col-sm">
         <div class="mb-3">
@@ -56,7 +56,7 @@ $controls = (object) [
             <x-error name='{{ $controls->rvalue->name }}'></x-error>
 
             @foreach($class::getAllMethodsWithRValues() as $method => $rvalues)                   
-            <template id='{{ $class::concatPrefix( "{$method}RValuesOptionsTemplate" ) }}'>
+            <template id='{{ $class::prefix( "{$method}RValuesOptionsTemplate" ) }}'>
                 <option disabled selected label="Choose a R-Value of <?= ucfirst($method) ?>..."></option>
                 @foreach($rvalues as $rvalue_name => $rvalue_amount)
                 <option data-amount="{{ $rvalue_amount }}" value="{{ $rvalue_name }}">{{ $rvalue_name }}</option>
